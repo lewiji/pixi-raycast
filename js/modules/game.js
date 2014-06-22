a71f02f139ffa4f6abed7660c3f53db1d954f5f9
Lewis Pollard <lewispollard@me.com>
2014-06-22 16:40:22 +0100

    Remove size from map

--- js/modules/game.js
+++ js/modules/game.js
@@ -16,10 +16,16 @@ renderer.view.className = 'game';
 
 // add the renderer view element to the DOM
 document.getElementById('gameContainer').appendChild(renderer.view);
-
 var loader = new PIXI.AssetLoader(['assets/img/redbrick.png', 
                                    'assets/img/pistol.png', 
-                                   'assets/img/skybox.png'], 
+                                   'assets/img/skybox.png',
+                                   'assets/img/wood.png',
+                                   'assets/img/purplestone.png',
+                                   'assets/img/mossy.png',
+                                   'assets/img/greystone.png',
+                                   'assets/img/bluestone.png',
+                                   'assets/img/eagle.png',
+                                   'assets/img/colorstone.png'], 
                                    true);
 loader.load();
 
@@ -31,18 +37,22 @@ function animate() {
 }
 
 function start () {
+  // Init textures
   Resources.init();
+  // add layers (DOCs)
   UI.addLayer('skybox');
   UI.addLayer('walls');
   UI.addLayer('gun');
+
   var sprite, walls = UI.getLayer('walls');
+  // Create wall 'slice' sprites (ie rays)
   for (var x = 0; x < Config.screenWidth; x++) {
     sprite = new PIXI.Sprite(Resources.get('texture')[0][4]);
     sprite.position.x = x;
     walls.addChild(sprite);
   }
 
-  var map = new Map(24);
+  var map = new Map();
   var player = new Player(22, 11.5, map);
 
   requestAnimFrame( animate );
